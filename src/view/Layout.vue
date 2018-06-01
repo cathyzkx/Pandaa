dca86ccc556459430d632fc35fbf95dcc0b94509
cathyzkx <463571952@qq.com>
2018-06-01 16:46:19 +0800

    pandaa

---
+++ src/view/Layout.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <el-container>
+      <Sidebar></Sidebar>
+      <el-container>
+        <el-header>
+          <Navbar></Navbar>
+        </el-header>
+        <el-main>
+          <AppMain></AppMain>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import { Navbar, Sidebar, AppMain } from '@/view'
+export default {
+  name: 'Layout',
+  components: {
+    Navbar,
+    Sidebar,
+    AppMain
+  }
+}
+</script>
+
+<style scoped>
+  .el-header {
+    background-color: #2C7CB1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    line-height: 900px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 860px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+    margin-left: 0px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
---
+++ src/view/Sidebar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+      <el-menu :default-openeds="['1']">
+        <el-submenu index="1">
+          <template slot="title"><i class="el-icon-star-on"></i>游戏助手</template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">狼人杀</el-menu-item>
+            <el-menu-item index="1-2">猜词</el-menu-item>
+            <el-menu-item index="1-3">最强大脑</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title"><i class="el-icon-menu"></i>小工具</template>
+          <el-menu-item-group>
+            <el-menu-item index="2-1">随机数</el-menu-item>
+            <el-menu-item index="2-2">人员抽签</el-menu-item>
+            <el-menu-item index="2-2">随机分组</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title"><i class="el-icon-setting"></i>设置</template>
+          <el-menu-item-group>
+            <el-menu-item index="3-1">游戏设置</el-menu-item>
+            <el-menu-item index="3-2">系统设置</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar'
+}
+</script>
+
+<style scoped>
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    color: #333;
+    text-align: left;
+    line-height: 900px;
+  }
+
+  .el-submenu {
+    color: #333;
+    text-align: left;
+    line-height: 900px;
+  }
+</style>
